6ebccc063c4dc6493dfe0588a1ba7a4132e89174
oltree <o.melex@bk.ru>
2023-05-28 20:25:02 +0300

    Added text-editor, finished genre edit page

---
+++ src/components/ui/slug-field/SlugField.tsx
@@ -0,0 +1,24 @@
+import { FC, memo } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import styles from './SlugField.module.scss';
+
+import { Field } from '../field';
+
+interface SlugFieldProps {
+  register: UseFormRegisterReturn<string>;
+  generate: VoidFunction;
+  placeholder: string;
+  error?: string | undefined;
+}
+
+export const SlugField: FC<SlugFieldProps> = memo(
+  ({ register, generate, placeholder, error }) => (
+    <div className='relative'>
+      <Field register={register} placeholder={placeholder} error={error} />
+      <p className={styles.badge} onClick={generate}>
+        generate
+      </p>
+    </div>
+  )
+);
