ea6b91d482d451be3aad9a75aa5c86971200e25d
oltree <o.melex@bk.ru>
2023-06-01 20:32:17 +0300

    Added trending movie page

---
+++ src/pages/trending.tsx
@@ -0,0 +1,40 @@
+import { GetStaticProps, NextPage } from 'next';
+
+import { Catalog } from '@/components/ui/catalog';
+
+import { IMovie } from '@/shared/interfaces/movie.interface';
+
+import { MovieService } from '@/services/movie.service';
+
+interface TrendingPageProps {
+  movies: IMovie[];
+}
+
+const TrendingPage: NextPage<TrendingPageProps> = ({ movies }) => (
+  <Catalog
+    title='Trending movies'
+    description='Trending movies'
+    movies={movies}
+  />
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const movies = await MovieService.getPopular();
+
+    return {
+      props: {
+        movies,
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        movies: [],
+        notFound: true,
+      },
+    };
+  }
+};
+
+export default TrendingPage;
